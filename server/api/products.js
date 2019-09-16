84a4d2a93203440e448b78739ae297537b6471a8
Monica Barboza <barboza.monica2@gmail.com>
2019-09-16 15:44:19 -0500

    create single product api

--- server/api/products.js
+++ server/api/products.js
@@ -4,3 +4,8 @@ module.exports = router
 router.get('/', (req, res, next) => {
   res.send('ALL PRODUCTS ARE HEREEEEEE')
 })
+
+router.get('/:id', (req, res, next) => {
+
+  res.send('single product data')
+})
